adb3c966e0a26e6369fa2342ca4e32660599d1c5
imen lakrib <lak12imen@gmail.com>
2023-07-13 15:39:06 -0700

    puts in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program.
- *
- * Return: Always 0 (indicating success).
- */
+  * main - Entry point
+  *
+  * Return: Always 0 (Success)
+  */
+
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
