fcdf33cc718cc35929dcd100bf8fe9ace8c854fa
imen lakrib <lak12imen@gmail.com>
2023-07-13 16:00:18 -0700

    desc

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,13 +2,14 @@
 
 /**
   * main - Prints "with proper grammar, but the outcome is
-  * a piece of art," followed by a newline.
+  *        a piece of art," followed by a newline.
   *
-  * Return : Always 0 (Success)
+  * Return: Always 0 (indicating success)
   */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
 }
