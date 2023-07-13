f6a608f401091b8ec03535ea79fa9173739c185f
imen lakrib <lak12imen@gmail.com>
2023-07-13 15:23:39 -0700

    purs in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program.
+ *
+ * Return: Always 0 (indicating success).
+ */
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
