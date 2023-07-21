9eee996a9720afa3272ccf06e41a636b6e045f6f
imen lakrib <lak12imen@gmail.com>
2023-07-21 14:30:53 -0700

    for

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * print_numbers - Prints all numbers in a range
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		_putchar(i);
+
+	_putchar('\n');
+}
