7af9eaf23209d6422f27aebddd1f02d45754e2a9
imen lakrib <lak12imen@gmail.com>
2023-07-21 14:32:42 -0700

    c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - Printing the numbers that occur most
+  */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+			continue;
+		putchar(i + '0');
+	}
+	putchar('\n');
+}
